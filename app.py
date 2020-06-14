2387a518f2c1ee482c6d1b42b6b6964ca8230979
Zachary Wilkins <38051315+zacharywilkins@users.noreply.github.com>
2020-06-14 16:10:31 -0700

    Adding Flask app that points to "Francisco" front end

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
-from flask import Flask, render_template, url_for
-from pathlib import Path
-
-current_directory = Path.cwd()
+from flask import Flask, render_template, url_for, jsonify
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello():
-    return "Welcome to Python Flask! Love, Z"
-
-
-home_path = str(current_directory / 'templates' / 'index.html')
 @app.route('/home')
 def home():
     url_for('static', filename='francisco.css')
-    # # redirecting to the *same page*, but without parameters
-    # from flask import redirect
-    # import requests
-    # return redirect(request.path, code=302)
     return render_template('index.html')
 
+@app.route('/augment', methods=['GET'])
+def augment():
+    data = {
+        "before_augmentation": "This augmentation service is great.",
+        "after_augmentation": [
+            "This augmentation service is excellent.",
+            "This is a great augmentation service.",
+            "This service for augmentation is superb.",
+            "This augmentation service is fantastic."
+        ]
+    }
+    response = jsonify(data)
+    return response
 
 if __name__ == "__main__":
     app.run()
